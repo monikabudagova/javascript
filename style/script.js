d0d088cc847c1af69bbb44f3cc08b60dd2c845b2
Oleg Nurzhanov <oleg@nastyle.pro>
2024-05-06 13:26:49 +0300

    update null

--- style/script.js
+++ style/script.js
@@ -1,11 +1,5 @@
 let result = 0
 
-function printNumber() {
-    if (true) {
-        document.getElementById("result").innerHTML += result + "<br>"
-        result++
-        setTimeout(printNumber, 10)
-    }
-}
 
-printNumber()
+
+document.getElementById("result").innerHTML = result
