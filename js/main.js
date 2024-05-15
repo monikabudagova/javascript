fb24c54f068edc89b45b49ff1f39c7e706135f32
monika <budagova.monika@yandex.ru>
2024-05-15 20:23:53 +0300

    hn7d

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-let messeges = 'привет мир'
-let num1 = 10
-let num2 =35
-
-let rusult = num1 + num2 //сложение
-let rusult2 = num1 - num2 //вычитание
-let rusult3 = num1 * num2 //умножение
-let rusult4 = num1 / num2 //деление
-
-console.log(rusult)  // 54
-console.log(rusult2)  // -34
-console.log(rusult3)  // 440
-console.log(rusult4)  // 0.9876
-
-console.log('ugpolh' * 1)
-
-console.log(`сумма двух чисел: ${rusult}`)  
-console.log(typeof(num1))
-console.log(typeof(messeges))
-
+let a = 4
+a++ //посстфиксная
+console.log(a)
